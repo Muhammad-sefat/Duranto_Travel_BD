49bf1f3190003db5a4fd57d834a07f01aee7077e
Muhammad Sefat <muhammadsefat55@gmail.com>
2025-01-20 08:19:05 +0600

    Complete Find Invoice section

--- Frontend/src/pages/FindInvoice.jsx
+++ Frontend/src/pages/FindInvoice.jsx
@@ -2,8 +2,18 @@ import React from "react";
 
 const FindInvoice = () => {
   return (
-    <div className="my-6">
-      <p>this is find invoice</p>
+    <div className="my-16 ml-12 text-left ">
+      <p className="text-2xl text-gray-700 my-5 uppercase">Fell At Home</p>
+      <h2 className="text-5xl text-gray-900 uppercase">we’re here for you</h2>
+      <hr className="w-[16%] my-8 size-2" />
+      <p className="w-[50%] text-xl text-gray-900">
+        Your happiness is our number one priority! Feel free to reach out with
+        any questions or concerns – We can’t wait to see you soon!
+      </p>
+      <div className="my-10 mt-16">
+        <p className="text-xl text-gray-900">40 Rue Nationale</p>
+        <p className="text-xl text-gray-900">75013 Paris, France</p>
+      </div>
     </div>
   );
 };
